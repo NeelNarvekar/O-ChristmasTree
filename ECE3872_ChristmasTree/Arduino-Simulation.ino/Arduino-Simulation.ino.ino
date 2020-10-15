3f2bf682aca80dca177497cafcf8f712cea824fd
Connor Truono <ctru14@gmail.com>
2020-10-15 11:56:24 -0400

    Updated Arduino testing for Sonar and Servo

--- ECE3872_ChristmasTree/Arduino-Simulation.ino/Arduino-Simulation.ino.ino
+++ ECE3872_ChristmasTree/Arduino-Simulation.ino/Arduino-Simulation.ino.ino
@@ -1,5 +1,11 @@
-#include <Adafruit_NeoPixel.h>
+#include <Adafruit_MCP4725.h>
+
+// TESTING FILE, NOT ACTUAL PROJECT CODE!!
+
 #include <Servo.h>
+
+
+//#include <Adafruit_NeoPixel.h>
 
 // Digital Pin Definitions
 #define ECHO          0
@@ -16,20 +22,100 @@
  // space for 11!
 #define STOP          12
 #define LED           13
+#define SONAR_TRIG    14
+#define SONAR_ECHO    15
+
+#define I2C_SDA       18 // DAC
+#define I2C_SCL       19 // DAC
 
 // Analog Pin Definitions
 #define POT           A0
 
 
+// LED Definitions for Demo!
+#define RED           7
+#define YELLOW        6
+#define GREEN         5 
+
+// Servo 
+Servo servo;
+
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(STATE_LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Servo Testing
+  servo.attach(SERVO_1_3); //*/
+  
+/*
+  // Sonar Testing
+  pinMode(SONAR_TRIG, OUTPUT);
+  pinMode(SONAR_ECHO, INPUT);
+  digitalWrite(SONAR_TRIG, LOW);
+  Serial.begin(115200); //*/
 }
 
+unsigned long cm;
+unsigned long tStart;
+unsigned long tEnd;
+
+int pos = 0;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Servo testing
+  digitalWrite(LED_BUILTIN, HIGH);
+    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  } //*/
+
+/*
+  // Sonar testing 
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, LOW);
+  delay(10);
+  tStart = millis();
+  cm = read_sonar_cm();
+  tEnd = millis();
+  Serial.print(tEnd - tStart);
+  Serial.println(" ms elapsed");
+  digitalWrite(LED_BUILTIN, LOW);
+  if (cm < 12) digitalWrite(RED, HIGH);
+  else { 
+    if (cm < 24) digitalWrite(YELLOW, HIGH); 
+    else digitalWrite(GREEN, HIGH); 
+  }
+  if (cm > 24) digitalWrite(GREEN, HIGH);
+  delay(400); //*/
+}
+
+// Blocking call to measure distance on sonar
+unsigned long read_sonar_cm() {
+  digitalWrite(SONAR_TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(SONAR_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(SONAR_TRIG, LOW);
+  unsigned long cm = pulseIn(SONAR_ECHO, HIGH, 300000) / 58;
+  Serial.print(cm);
+  Serial.print(" cm - ");
+  return cm; // Distance in cm
+}
+
+
+void blink() {
   digitalWrite(STATE_LED, LOW);
-  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(600);
   digitalWrite(STATE_LED, HIGH);
-  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(300);
 }
